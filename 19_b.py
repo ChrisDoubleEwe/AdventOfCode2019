c2441e5e095c99b61d89001bde16dce1af9da75f
Chris Walsh <chris@offwhitehouse.co.uk>
2021-01-18 22:08:03 +0000

    Day 19

---
+++ 19_b.py
@@ -0,0 +1,254 @@
+import sys
+from printf import printf
+import copy
+
+with open("19_input.txt") as f:
+    content = f.readlines()
+
+
+prog = []
+content = [x.strip() for x in content]
+for line in content:
+  values = line.split(',')
+  for v in values:
+    prog.append(long(v))
+
+for i in range(0, 1000):
+  prog.append(0)
+
+pc = 0
+base = 0
+
+orig_prog = list(prog)
+prog = list(orig_prog)
+
+def get_arg(arg):
+  global pc
+  global prog
+  global base
+  global p1_mode
+  global p2_mode
+  global p3_mode
+
+  #print "GET ARG " + str(arg) + " mode = " + str(p1_mode) + " base=" + str(base)
+  if arg == 1:
+    if p1_mode == 0:
+      p = prog[prog[pc+1]]
+    elif p1_mode == 2:
+      p = prog[prog[pc+1]+base] 
+    else:
+      p = prog[pc+1]
+  if arg == 2:
+    if p2_mode == 0:
+      p = prog[prog[pc+2]]
+    elif p2_mode == 2:
+      p = prog[prog[pc+2]+base]
+    else:
+      p = prog[pc+2]
+  return p
+
+def get_loc(arg):
+  global pc
+  global prog
+  global base
+  global p1_mode
+  global p2_mode
+  global p3_mode
+
+  if arg == 1:
+    if p1_mode == 0:
+      p = prog[pc+1]
+    else:
+      p = prog[pc+1]+base
+      #print "Returning : " + str(p)
+  if arg == 3:
+    if p3_mode == 0:
+      p = prog[pc+3]
+    else:
+      p = prog[pc+3]+base
+      #print "Returning : " + str(p)
+  return p
+
+def run(input_val):
+ global pc
+ global prog
+ global base
+ global p1_mode
+ global p2_mode
+ global p3_mode
+ pc = 0
+ base = 0
+ prog = list(orig_prog)
+
+ while True:
+  #print prog
+  op = prog[pc]
+
+  # Decode 5-digit op-code
+  op_str = str(op)
+  while len(op_str) < 5:
+    op_str = '0' + op_str
+
+  op_both = op_str[3] + op_str[4]
+  op_both_int = long(op_both)
+
+  op_code = long(op_str[3]+op_str[4])
+  p1_mode = long(op_str[2])
+  p2_mode = long(op_str[1])
+  p3_mode = long(op_str[0])
+
+  #print op_str
+  #print "        " + str(prog[pc+1]) + " " + str(prog[pc+2]) + " " + str(prog[pc+3]) 
+
+  if op == 99:
+      #print "EXIT"
+      sys.exit()
+
+  if op_code == 1:
+    p1 = get_arg(1)
+    p2 = get_arg(2)
+    out = get_loc(3)
+    prog[out] = p1 + p2
+    #print "   1: Add  (" + str(p1) + " + " + str(p2) + ") to " + str(out)
+    pc = pc +4 
+    continue
+
+  if op_code == 2:
+    p1 = get_arg(1)
+    p2 = get_arg(2)
+    out = get_loc(3)
+    prog[out] = p1 * p2
+    #print "   2: Mult (" + str(p1) + " * " + str(p2) + ") to " + str(out)
+    pc = pc +4
+    continue
+
+  if op_code == 3:
+    p1 = get_loc(1)
+    prog[p1] = input_val.pop(0)
+    #print "   3: Set [" + str(prog[pc+1]) + "] to " + str(input_val)
+    pc = pc +2
+    continue
+
+  if op_code == 4:
+    p1 = get_arg(1)
+    #print "   4: OUTPUT " + str(p1)
+    pc = pc +2
+    return p1
+    continue
+
+  # JUMP-IF-TRUE
+  if op_code == 5:
+    p1 = get_arg(1)
+    p2 = get_arg(2)
+    if p1 != 0:
+      #print "   5: JUMP TAKEN"
+      pc = p2
+    else:
+      #print "   5: Jump skipped"
+      pc = pc + 3
+    continue
+
+  # JUMP-IF-FALSE
+  if op_code == 6:
+    p1 = get_arg(1)
+    p2 = get_arg(2)
+    if p1 == 0:
+      #print "   6: JUMP TAKEN"
+      pc = p2
+    else:
+      #print "   6: Jump skipped"
+      pc = pc + 3
+    continue
+
+  # LESS THAN
+  if op_code == 7:
+    p1 = get_arg(1)
+    p2 = get_arg(2)
+    p3 = get_loc(3)
+    if p1 < p2:
+      #print "   7: Less Than True"
+      prog[p3] = 1
+    else:
+      #print "   7: Less Than False"
+      prog[p3] = 0
+    pc = pc +4
+    continue
+
+  # EQUALS
+  if op_code == 8:
+    p1 = get_arg(1)
+    p2 = get_arg(2)
+    p3 = get_loc(3)
+    if p1 == p2:
+      #print "   8: Equals True, put 1 in " + str(p3)
+      prog[p3] = 1
+    else:
+      #print "   8: Equals False, put 0 in " + str(p3)
+      prog[p3] = 0
+    pc = pc +4
+    continue
+
+  # ADJUST RELATIVE BASE OFFSET
+  if op_code == 9:
+    p1 = get_arg(1)
+    base = base + p1
+    pc = pc +2
+    continue
+
+width = 100
+x = 0
+y = 3
+while True:
+  print "x: " + str(x) + " ; y = " + str(y)
+
+  tl = []
+  tl.append(x)
+  tl.append(y)
+
+  tr = []
+  tr.append(x+width-1)
+  tr.append(y)
+
+  bl = []
+  bl.append(x)
+  bl.append(y+width-1)
+
+  br = []
+  br.append(x+width-1)
+  br.append(y+width-1)
+
+  o_tl = run(tl)
+  if o_tl == 0:
+    x+= 1
+    continue
+  print "GOT TOP LEFT"
+
+  o_tr = run(tr)
+  if o_tr == 0:
+    y+=1
+    continue
+  print "GOT TOP RIGHT"
+
+  o_bl = run(bl)
+  if o_bl == 0:
+    x+=1
+    continue
+  print "GOT BOTTOM LEFT"
+
+  o_br = run(br)
+  if o_br == 0:
+    y+=1
+    continue
+  print "GOT BOTTOM RIGHT"
+
+
+
+  if o_tl == o_tr == o_bl == o_br == 1:
+    print "GOT IT!!!!"
+    print "x: " + str(x) + " ; y = " + str(y)
+    res = (x * 10000) + y
+    print "Part 2: " + str(res)
+    exit()
+
+
+  
